d526770c9da819450a9b23b57a954ac0577fedf2
花木凋零成兰 <18570354653@163.com>
2024-11-08 23:54:12 +0800

    openfeign 配置类的方式配置日志

---
+++ teacher-nacos/src/main/java/top/ytazwc/alibaba/config/FeignConfig.java
@@ -0,0 +1,30 @@
+package top.ytazwc.alibaba.config;
+
+import feign.Logger;
+import feign.Request.Options;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 花木凋零成兰
+ * @title FeignConfig
+ * @date 2024-11-08 23:47
+ * @package top.ytazwc.alibaba.config
+ * @description 配置openfeign日志和超时与读取时间
+ */
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Options options() {
+        // 设置连接和读取超时时间
+        return new Options(5000, 10000);
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        // 设置日志级别为详细
+        return Logger.Level.FULL;
+    }
+
+}
--- teacher-nacos/src/main/java/top/ytazwc/alibaba/feign/StudentFeign.java
+++ teacher-nacos/src/main/java/top/ytazwc/alibaba/feign/StudentFeign.java
@@ -2,6 +2,7 @@ package top.ytazwc.alibaba.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import top.ytazwc.alibaba.config.FeignConfig;
 
 /**
  * @author 花木凋零成兰
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @package top.ytazwc.alibaba.feign
  * @description 远程调用 Student 服务
  */
-@FeignClient(value = "student", path = "/student") // 值为student服务名
+@FeignClient(value = "student", path = "/student", configuration = FeignConfig.class) // 值为student服务名
 public interface StudentFeign {
 
     @GetMapping("/get")
