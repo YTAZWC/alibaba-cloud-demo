fc984bcda7125f70cdea4c862682e4484afe6d36
花木凋零成兰 <18570354653@163.com>
2024-11-10 22:42:20 +0800

    seata 初始工程

--- seata-02/src/main/java/top/ytazwc/alibaba/seata02/Seata02Application.java
+++ seata-02/src/main/java/top/ytazwc/alibaba/seata02/Seata02Application.java
@@ -1,13 +1,13 @@
-package top.ytazwc.alibaba.seata01;
+package top.ytazwc.alibaba.seata02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Seata01Application {
+public class Seata02Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Seata01Application.class, args);
+        SpringApplication.run(Seata02Application.class, args);
     }
 
 }
