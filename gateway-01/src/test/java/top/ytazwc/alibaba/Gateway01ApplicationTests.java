6e93611f5b6cb7048b5065bf286bb197978f9fed
花木凋零成兰 <18570354653@163.com>
2024-11-11 15:07:24 +0800

    gateway-初始工程+导入依赖

---
+++ gateway-01/src/test/java/top/ytazwc/alibaba/Gateway01ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ytazwc.alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
